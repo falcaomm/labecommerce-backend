c211f7a61ffa8848bdca5e52bedcb9e2107ca8c9
falcaomm <manu.m.falcao@gmail.com>
2023-03-16 19:14:24 -0300

    feedback4

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { users, products, purchases } from "./database"
-import { Category } from "./types"
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { products, users, purchases } from './database'
+import { Category, TUser, TProduct, TPurchase } from "./types"
 import {
     getProductById, 
     getAllProducts,
@@ -10,16 +12,88 @@ import {
     getAllPurchasesFromUserId
 } from "./database"
 
-createUser("u003", "beltrano@email.com", "beltrano99")
+const app = express()
 
-getAllUsers()
+app.use(express.json())
+app.use(cors())
 
-createProduct("p004", "Monitor HD", 800, Category.ELECTRONICS)
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
 
-getAllProducts()
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
 
-console.log(getProductById('p004'))
+app.get('/users', (req: Request, res: Response) => {
+    res.status(200).send(users)
+})
 
-console.log(queryProductsByName("PRO"))
+app.get('/products', (req: Request, res: Response) => {
+    res.status(200).send(products)
+})
 
-console.log(getAllPurchasesFromUserId("02"));
+app.get('/products/search', (req: Request, res: Response) => {
+    const findForName = req.query.name as string
+
+    const resust = findForName ?
+        products.filter(item => item.name.toLowerCase().includes(findForName.toLowerCase()))
+        : products;
+
+    res.status(200).send(resust)
+})
+
+app.post('/users', (req: Request, res: Response) => {
+
+    const newUser: TUser= {
+        id: req.body.id,
+        email: req.body.email,
+        password: req.body.password
+    }
+    users.push(newUser)
+
+    res.status(201).send('Cadastro realizado com sucesso')
+})
+
+app.post('/products', (req: Request, res: Response) => {
+
+    const newProduct: TProduct= {
+        id: req.body.id,
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category
+    }
+    products.push(newProduct)
+
+    res.status(201).send('Produto cadastrado com sucesso')
+})
+
+app.post('/purchase', (req: Request, res: Response) => {
+
+    const newPurchase: TPurchase= {
+        userId : req.body.userId,
+        productId: req.body.productId,
+        quantity: req.body.quantity,
+        totalPrice: req.body.totalPrice
+    }
+    purchases.push(newPurchase)
+
+    res.status(201).send('Compra realizada com sucesso')
+})
+
+console.log(purchases);
+
+
+// createUser("u003", "beltrano@email.com", "beltrano99")
+
+// getAllUsers()
+
+// createProduct("p004", "Monitor HD", 800, Category.ELECTRONICS)
+
+// getAllProducts()
+
+// console.log(getProductById('p004'))
+
+// console.log(queryProductsByName("PRO"))
+
+// console.log(getAllPurchasesFromUserId("02"));
